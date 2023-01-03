c837b3ae83ed87f3b402e72da7ac20ea4a5db3f7
ElishaiGusman <121834475+ElishaiGusman@users.noreply.github.com>
2023-01-03 03:27:28 +0200

    Add files via upload

---
+++ Scripts/MonoBehaviours/Character.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Character : MonoBehaviour
+{
+	public HitPoints hitPoints;
+	public float maxHitPoints;
+	public float startingHitPoints;
+
+	public enum CharacterCategory
+	{
+		PLAYER,
+		ENEMY
+	}
+
+	public CharacterCategory characterCategory;
+}
---
+++ Scripts/MonoBehaviours/Player.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Character
+{
+	public HealthBar healthBarPrefab;
+	HealthBar healthBar;
+
+	public void Start()
+	{
+		hitPoints.value = startingHitPoints;
+		healthBar = Instantiate(healthBarPrefab);
+		healthBar.character = this;
+	}
+
+	void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.gameObject.CompareTag ("CanBePickedUp")) 
+		{
+			Item hitObject = collision.gameObject.GetComponent<Consumable>().item;
+
+			if (hitObject != null) 
+			{
+				bool shouldDisappear = false;
+
+				print ("it: " + hitObject.objectName);
+
+				switch (hitObject.itemType) 
+				{
+					case Item.ItemType.COIN:
+						shouldDisappear = true;
+						break;
+					case Item.ItemType.HEALTH:
+						shouldDisappear = AdjustHitPoints(hitObject.quantity);
+						break;
+					default:
+						break;
+				}
+				if (shouldDisappear) 
+				{
+					collision.gameObject.SetActive (false);
+				}
+			}
+		}
+	}
+
+	public bool AdjustHitPoints(int amount)
+	{
+		if (hitPoints.value < maxHitPoints) 
+		{
+			hitPoints.value += amount;
+			print("Adjusted HP by: " + amount + ". New calue: " +
+				hitPoints.value);
+			return true;
+		}
+		return false;
+	}
+		
+}
